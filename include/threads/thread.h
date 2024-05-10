42b24a696f7683937fa8278cbae3ffe80bda35e4
yooju00 <yooju00@naver.com>
2024-05-10 11:35:33 +0000

    thread_sleep function

--- include/threads/thread.h
+++ include/threads/thread.h
@@ -91,6 +91,7 @@ struct thread {
 	enum thread_status status;          /* Thread state. */
 	char name[16];                      /* Name (for debugging purposes). */
 	int priority;                       /* Priority. */
+	int64_t local_ticks;				/* Local Ticks */
 
 	/* Shared between thread.c and synch.c. */
 	struct list_elem elem;              /* List element. */
